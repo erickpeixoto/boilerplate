07502813e8b791828c6c7a2d9fa59a858bfa6bfd
erickpeixoto <erickepeixoto@gmail.com>
2024-02-21 12:43:04 -0300

    feat: Add Theme Management

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
 import { Button } from "@/app/components/ui/button";
+import { ThemeToggle } from "@/app/components/theme-toggle";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <ThemeToggle />
       <Button>Hey there!</Button>
     </main>
   );
