e9f205e55e212e73e0db3ab62e2ec38a7c1a9d35
erickpeixoto <erickepeixoto@gmail.com>
2024-02-23 14:11:11 -0300

    chore: Add TRPC, Drizzle, React Query and more

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,17 @@
-import Image from "next/image";
 import { Button } from "@/app/components/ui/button";
 import { ThemeToggle } from "@/app/components/theme-toggle";
+import { api } from "@/trpc/server";
 
-export default function Home() {
+export default async function Home() {
+  const hi = await api.post.hello.query({
+    text: "from the Erick's Boilerplate",
+  });
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <ThemeToggle />
-      <Button>Hey there!</Button>
+      <Button>Hey there, I've came out of the Shadcn-UI box!</Button>
+      <h1 className="text-4xl font-bold">{hi.greeting}</h1>
     </main>
   );
 }
